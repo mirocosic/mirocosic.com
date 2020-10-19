4cb22d37e085ff7a81290b855486ffac0c2fa18c
Miro <mirocosic@gmail.com>
2020-10-19 12:59:33 +0200

    about cleanup

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,14 @@ import { Page } from "../components"
 
 export default () => (
   <Page>
-    <h1 className="display-4 text-center">About</h1>
-      <p><strong>Favourite music:</strong> good music :) </p>
-      <p>Personality type: INFJ</p>
-    <h5>Travel Map</h5>
+    <div className="d-flex flex-column justify-content-center">
+      <h1 className="display-4 text-center">About</h1>
+        <p><strong>Favourite music:</strong> good music :) </p>
+        <p>Personality type: INFJ</p>
+
+        <a href="https://www.codewars.com/users/mirocosic" rel="noopener noreferrer" target="_blank">
+          <img src="https://www.codewars.com/users/mirocosic/badges/small" alt="codewars-rank" />
+        </a>
+      </div>
   </Page>
 )
